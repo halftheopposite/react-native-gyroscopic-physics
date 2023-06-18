7f14253fd74c14165bee5e1e55b7c499eeea4cb9
Aymeric Chauvin <contact@halftheopposite.dev>
2023-06-18 22:31:46 +0200

    Add bubble dragging

--- src/BubblesContainer.tsx
+++ src/BubblesContainer.tsx
@@ -1,6 +1,12 @@
-import { Canvas, Fill, SweepGradient, vec } from "@shopify/react-native-skia";
-import { Engine } from "matter-js";
-import { useEffect, useRef, useState } from "react";
+import {
+  Canvas,
+  Fill,
+  SweepGradient,
+  useTouchHandler,
+  vec,
+} from "@shopify/react-native-skia";
+import { Body, Composite, Engine, Query } from "matter-js";
+import { memo, useEffect, useRef, useState } from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
 import { GradientButton } from "./GradientButton";
 import { Bubble } from "./bodies";
@@ -31,7 +37,7 @@ export function BubblesContainer() {
       />
 
       {/* UI layout */}
-      <View style={styles.contentContainer}>
+      <View style={styles.contentContainer} pointerEvents="box-none">
         <GradientButton
           title="More bubbles!"
           style={styles.button}
@@ -84,9 +90,66 @@ function BubblesBackground(props: {
     update();
   }, []);
 
+  // Handle touch events to move bodies
+  const [draggingBody, setDraggingBody] = useState<Body | null>(null);
+
+  const touchHandler = useTouchHandler(
+    {
+      onStart: ({ x, y }) => {
+        if (draggingBody) {
+          return;
+        }
+
+        const bodies = Query.point(Composite.allBodies(engine.world), {
+          x,
+          y,
+        });
+
+        if (!bodies || bodies.length === 0) {
+          return;
+        }
+
+        const firstBody = bodies[0];
+        if (firstBody.isStatic) {
+          return;
+        }
+
+        setDraggingBody(bodies[0]);
+      },
+      onActive: (info) => {
+        if (!draggingBody) {
+          return;
+        }
+
+        draggingBody.isStatic = true;
+
+        Body.setPosition(draggingBody, {
+          x: info.x,
+          y: info.y,
+        });
+      },
+      onEnd: (info) => {
+        if (!draggingBody) {
+          return;
+        }
+
+        draggingBody.isStatic = false;
+
+        Body.applyForce(
+          draggingBody,
+          { x: info.x, y: info.y },
+          { x: info.velocityX / 10000, y: info.velocityY / 10000 }
+        );
+
+        setDraggingBody(null);
+      },
+    },
+    [draggingBody]
+  );
+
   return (
-    <View style={styles.container} pointerEvents="none">
-      <Canvas style={styles.canvas}>
+    <View style={styles.container}>
+      <Canvas style={styles.canvas} onTouch={touchHandler}>
         <Fill>
           <SweepGradient
             c={vec(-45, 0)}
@@ -164,28 +227,30 @@ function BubblesBackground(props: {
   );
 }
 
-function Bubbles(props: {
-  engine: Engine;
-  spawnPoint: { x: number; y: number };
-  count: number;
-}) {
-  const { engine, spawnPoint, count } = props;
-  const array = [...Array(count).keys()];
+const Bubbles = memo(
+  (props: {
+    engine: Engine;
+    spawnPoint: { x: number; y: number };
+    count: number;
+  }) => {
+    const { engine, spawnPoint, count } = props;
+    const array = [...Array(count).keys()];
 
-  return (
-    <>
-      {array.map((value, index) => (
-        <Bubble
-          key={`bubble-${index}`}
-          engine={engine}
-          x={spawnPoint.x}
-          y={spawnPoint.y}
-          radius={BUBBLE_RADIUS}
-        />
-      ))}
-    </>
-  );
-}
+    return (
+      <>
+        {array.map((value, index) => (
+          <Bubble
+            key={`bubble-${index}`}
+            engine={engine}
+            x={spawnPoint.x}
+            y={spawnPoint.y}
+            radius={BUBBLE_RADIUS}
+          />
+        ))}
+      </>
+    );
+  }
+);
 
 const styles = StyleSheet.create({
   container: {
